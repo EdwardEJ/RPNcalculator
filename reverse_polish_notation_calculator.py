c35816c81fd80fa8274dc180e3e213cf59bab679
A <jesus.e.estrada@hotmail.com>
2021-08-29 04:39:37 -0400

    rpn algo under num_operator_or_letter_check

--- reverse_polish_notation_calculator.py
+++ reverse_polish_notation_calculator.py
@@ -1,52 +1,65 @@
 import operator
 import argparse
 import sys
-import pylint.lint
+# import pylint.lint
 
-pylint_opts = ['basics.py','--load-plugins=pylint.extensions.mccabe','--rcfile=~/.pylintrc']
+# pylint_opts = ['basics.py','--load-plugins=pylint.extensions.mccabe','--rcfile=~/.pylintrc']
 
-pylint.lint.Run(pylint_opts)
+# pylint.lint.Run(pylint_opts)
 
 def new_calc_exit_calc(expr):
-    '''
-    will either exit or create a new calculation with an exmpty stack
-    '''
     if expr == 'q':
         print('Exit')
         sys.exit()
     elif expr == 'n':
         print('New Calculation')
         continue_expr = False
-        return continue_expr, new_calc_exit_calc()
+        return continue_expr, reverse_polish_notation_calc()
+
+def num_operator_or_letter_check(expr, operators, stack, ops):
+    if not_num_or_operator(expr, operators):
+        print('Cannot add letters')
+    elif expr.isdigit():
+        stack.append(int(expr))
+    elif expr not in operators:
+        stack.append(float(expr))
+    elif expr in operators:
+        if len(stack) <= 1:
+            print("Need at least 2 values before calculating")
+            pass
+        else:
+            expr_operator = expr
+            first_val = stack.pop()
+            second_val = stack.pop()
+            result = ops[f'{expr_operator}'](second_val, first_val)
+            stack.append(result)
+            print(result)
 
 def calc_more_than_one_expr(expr, stack, operators, ops):
-    '''
-    for calculating an argument with more than one expression ex: 4 3 2 + +
-    '''
     for num in expr.split():
-        if num.isalpha() and num not in operators:
-            print('Cannot add letters')
-        elif num in operators and len(stack) < 2:
-            print("Need at least 2 values before calculating")
-            break
-        else:
-            if num.isdigit():
-                stack.append(int(num))
-            elif num not in operators:
-                stack.append(float(num))
-            elif num in operators:
-                expr_operator = num
-                first_val = stack.pop()
-                second_val = stack.pop()
-                result = ops[f'{expr_operator}'](second_val, first_val)
-                stack.append(result)
-    print(stack[-1])
+        num_operator_or_letter_check(num, operators, stack, ops)
+        # if not_num_or_operator(num, operators):
+        #     print('Cannot add letters')
+        # elif num.isdigit():
+        #     stack.append(int(num))
+        # elif num not in operators:
+        #     stack.append(float(num))
+        # elif num in operators:
+        #     if len(stack) <= 1:
+        #         print("Need at least 2 values before calculating")
+        #         pass
+        #     else:
+        #         expr_operator = num
+        #         first_val = stack.pop()
+        #         second_val = stack.pop()
+        #         result = ops[f'{expr_operator}'](second_val, first_val)
+        #         stack.append(result)
+        #         print(result)
 
+def not_num_or_operator(expr, operators):
+    return expr.isalpha() and expr not in operators
 
 def reverse_polish_notation_calc():
-    '''
-    calculates using postfix notation
-    '''
     ops = { "+": operator.add,
            "-": operator.sub,
            "*": operator.mul,
@@ -64,26 +77,13 @@ def reverse_polish_notation_calc():
         if len(cmd_line_expr) > 1:
             calc_more_than_one_expr(cmd_line_expr, stack, operators, ops)
         else:
-            if cmd_line_expr.isalpha() and cmd_line_expr not in operators:
-                print('Cannot add letters')
-                continue
+            # not_num_or_operator(cmd_line_expr, operators)
+            # if cmd_line_expr.isalpha() and cmd_line_expr not in operators:
+            #     print('Cannot add letters')
 
-            if cmd_line_expr in operators and len(stack) >= 2:
-                expr_operator = cmd_line_expr
-                first_val = stack.pop()
-                second_val = stack.pop()
-                result = ops[f'{expr_operator}'](second_val, first_val)
-                stack.append(result)
-                print(result)
-            elif cmd_line_expr in operators and len(stack) < 2:
-                print('Result of last expression: ',f"{stack[-1]}")
-                continue
-            elif cmd_line_expr.isdigit():
-                print(cmd_line_expr)
-                stack.append(int(cmd_line_expr))
-            else:
-                print(cmd_line_expr)
-                stack.append(float(cmd_line_expr))
+            print(cmd_line_expr)
+
+            num_operator_or_letter_check(cmd_line_expr, operators, stack, ops)
 reverse_polish_notation_calc()
 
 def main():
